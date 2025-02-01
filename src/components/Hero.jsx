ae8b4e68f4e22e87d38c8b8017d43b34461389a1
Jonathan-Ultini <jonathanultini@gmail.com>
2025-02-01 13:07:04 +0100

    set text, fix videos autoplay

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,28 @@ const Hero = () => {
           <video
             ref={nextVideoRef}
             src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id='next-video'
+            className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
           />
+          <video
+            src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+            autoPlay
+            loop
+            muted
+            className='absolute left-0 top-0 size-full object-cover object-center'
+            onLoadedData={handleVideoLoaded}
+          />
+        </div>
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className='special-font hero-heading text-blue-100'>redefi<b>n</b>e</h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">Enter the Metagame Layer <br />Unleash the PLay Economy</p>
+          </div>
         </div>
       </div>
     </div>
